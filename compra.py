ef34dfc77e1c2761dfd5ee4a2d31c182ed90bb89
Neves77-ng <carloshenriquecclcla@gmail.com>
2026-08-31 22:57:15 -0300

    Atualiza compra.py

---
+++ compra.py
@@ -0,0 +1,6 @@
+produto = str(input("Informe o nome do produto: "))
+preco = float(input("Informe o valor do produto: "))
+quantidade = int(input("Informe a quantidade desejada: "))
+valor_total = preco * quantidade
+
+print(f"--------------------------\nRESUMO DO PEDIDO\nProduto: {produto}\nValor unitário: R$ {preco:.3f}\nQuantidade: {quantidade}\nValor total: R$ {valor_total:.3f}\n--------------------------")
