4ee621f666c00d14aa2b1e00930907e9476c7102
Neves77-ng <carloshenriquecclcla@gmail.com>
2026-09-01 20:20:12 -0300

    teste

--- compra.py
+++ compra.py
@@ -4,3 +4,5 @@ quantidade = int(input("Informe a quantidade desejada: "))
 valor_total = preco * quantidade
 
 print(f"--------------------------\nRESUMO DO PEDIDO\nProduto: {produto}\nValor unitário: R$ {preco:.2f}\nQuantidade: {quantidade}\nValor total: R$ {valor_total:.2f}\n--------------------------")
+
+#att teste
