5401555771b2d437acf0d168fefe373f9d1dbcd6
Carlos Henrique Neves de Lima <CarlosHenrique@MacBook-Air-de-Livia.local>
2026-09-05 17:16:01 -0300

    temp

---
+++ estudos/Estudos.py
@@ -0,0 +1,12 @@
+temperatura = float(input("Insira a temperatura atual:"))
+
+if temperatura > 25:
+    print("Está quente pa porra")
+
+elif temperatura < 15:
+    print("Oh frio do djabo")
+
+else:
+    print("Climinha gostoso!!")
+
+
